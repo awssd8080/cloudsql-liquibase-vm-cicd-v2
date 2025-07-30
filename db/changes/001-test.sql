ec3b3e46f20fa3771bf04861b1d7103477e35982
awssd8080 <awssreekanth04@gmail.com>
2025-07-30 09:38:02 -0400

    Update 001-test.sql

--- db/changes/001-test.sql
+++ db/changes/001-test.sql
@@ -1,3 +1,7 @@
 --liquibase formatted sql
 --changeset admin:001
 CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name VARCHAR(50));
+
+--liquibase formatted sql
+--changeset sree:001
+CREATE TABLE IF NOT EXISTS test_table11 (id SERIAL PRIMARY KEY, name VARCHAR(50));
